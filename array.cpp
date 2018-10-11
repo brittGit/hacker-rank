2967e99de44b386d65f8f50b8cac048e7dcc8b45
Brittany <britt.garcia123@gmail.com>
2018-10-10 23:03:21 -0700

    changed commenting in array

--- array.cpp
+++ array.cpp
@@ -7,7 +7,6 @@ using namespace std;
 
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
   int inputSize = 0;
   cout << "How big you want the array: ";
   cin >> inputSize;
